11f900ddbaf553de056af6431bb75249b7bb7f7f
Mateusz Łukaszenko <mateuszl@man.poznan.pl>
2017-10-25 16:32:47 +0200

    working topics activity. During adding new topic activity

--- app/src/main/java/com/mateuszl/reporterapp/activities/EditTopicActivity.java
+++ app/src/main/java/com/mateuszl/reporterapp/activities/EditTopicActivity.java
@@ -1,10 +1,14 @@
 package com.mateuszl.reporterapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.mateuszl.reporterapp.R;
+import com.mateuszl.reporterapp.model.Topic;
 
 public class EditTopicActivity extends AppCompatActivity {
 
@@ -15,8 +19,28 @@ public class EditTopicActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_topic);
 
-
         acceptBtn = (ImageButton) findViewById(R.id.accept_btn);
 
+        acceptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (true) { //todo jesli dane w formularzu są poprawne
+
+                    createNewTopic();
+
+                    Intent intent = new Intent(getApplicationContext(), TopicsActivity.class);
+                    intent.putExtra("user_name", "mocked userName");
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Złe dane.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void createNewTopic() {
+        Topic topic = new Topic();
+        //// TODO: 25.10.2017 pusznięcie topica na serwer
     }
 }
